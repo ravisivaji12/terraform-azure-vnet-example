390a82de2fc0602f210541975defe479ffb5c1fb
ravisivaji12 <ravi.sivaji@outlook.com>
2025-04-10 03:01:40 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "remote" {
+    organization = "SivajiRaavi"  # Replace with your Terraform Cloud organization name
+
+    workspaces {
+      name = "terratest-azure-vnet-poc"  # Replace with your workspace name
+    }
+  }
+}
 provider "azurerm" {
   features {}
   subscription_id = "abd34832-7708-43f9-a480-e3b7a87b41d7" #var.subscription_id
